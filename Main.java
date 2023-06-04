0fb6d4f6b67ab2ad20c8d8b4b0c3b47b7d883d2d
CoochiecooBerrauslu <113683902+CoochiecooBerrauslu@users.noreply.github.com>
2023-06-04 17:14:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,8 +7,11 @@ public class Main {
         int secondNumber = 0;
         
         for(int i = 999; i>= 100; i--){
+            // generating all possible products of two 3-digit numbers
             for(int j = i; j >= 100; j--){
                 int product = i * j;
+                // if the method returns true and product is bigger that the largest palindrome 
+                // largest palindrome equals to product
                 if(isPalindrome(product) && product> largestPalindrome){
                     largestPalindrome = product;
                     firstNumber = i;
@@ -24,11 +27,16 @@ public class Main {
     public static boolean isPalindrome(int number){
         int reversedNumber = 0;
         int originalNumber = number;
+        // reversing the original number in order to compare with the original one.
         while(number != 0){
+            // this gives us the rightmost digit
             int digit = number % 10;
+            // an then we add it to the beginning
             reversedNumber = reversedNumber * 10 + digit;
+            // and by dividing with then the rightmost digit disappears
             number /= 10;
         }
+        // the loop will continiue until the number becomes zero.
         return originalNumber == reversedNumber;
 
     }
